53ef117ba1d185b3840b9b19d1a7b0940006a845
vitorhugo123-a <bugao.macedo7@gmail.com>
2026-09-15 14:52:28 -0300

    projeto1.py

---
+++ projeto1.py
@@ -0,0 +1,34 @@
+import pickle
+from datetime import datetime
+import faker
+
+class comanda:
+
+    def __init__(self,numero,cliente):
+        #atributos
+        self.numero=numero
+        self.cliente=cliente
+        # registra a data e hora da abertura
+        self.data_hora_abertura=datetime.now()
+        #lista pra armazenar itens
+        self.refeicoes=[]
+        self.bebidas=[]
+
+    def adicionar_refeicao(self,refeicao):
+        self.refeicoes.append(refeicao)
+
+    def remover_refeicao(self,refeicao):
+        if refeicao in self.refeicoes:
+            self.refeicoes.remove(refeicao)
+            return True
+        return False
+
+    def adicionar_bebida(self,bebida):
+        self.bebidas.append(bebida)
+
+    def remover_bebida(self,bebida):
+        if bebida in self.bebidas:
+            return True
+        return False
+
+comandas_abertas={}
